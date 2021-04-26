e87ffed777380ae2cd20ffc63a63e2433ab0d03a
Michael Diamond <dimo414@gmail.com>
2021-04-26 02:46:19 -0700

    Implement cache TTLs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,13 @@ use clap::{Arg, App};
 use serde::{Serialize, Deserialize};
 use std::hash::{Hash, Hasher};
 use std::fs::File;
+use std::time::{Duration, Instant, SystemTime};
+
+// See https://doc.rust-lang.org/std/fs/fn.soft_link.html
+#[cfg(windows)]
+use std::os::windows::fs::symlink_file as symlink;
+#[cfg(unix)]
+use std::os::unix::fs::symlink;
 
 // Describes a command to be cached. Is used as the cache key.
 #[derive(Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
@@ -61,10 +68,13 @@ struct Invocation {
     stdout: Vec<u8>,
     stderr: Vec<u8>,
     status: i32,
+    runtime: Duration,
+    completed: SystemTime,
 }
 
 struct Cache {
-    dir: PathBuf,
+    key_dir: PathBuf,
+    data_dir: PathBuf,
 }
 
 impl Cache {
@@ -72,10 +82,14 @@ impl Cache {
         let mut dir = root_dir.map(PathBuf::from).unwrap_or_else(std::env::temp_dir);
         // Note the cache is invalidated when the minor version changes
         dir.push(format!(".bkt-{}.{}-cache", env!("CARGO_PKG_VERSION_MAJOR"), env!("CARGO_PKG_VERSION_MINOR")));
+        let mut key_dir = dir.clone();
+        key_dir.push("keys");
         if let Some(scope) = scope {
-            dir.push(scope);
+            key_dir.push(scope);
         }
-        Cache{ dir }
+        let mut data_dir = dir;
+        data_dir.push("data");
+        Cache{ key_dir, data_dir }
     }
 
     #[cfg(not(feature = "debug"))]
@@ -99,7 +113,7 @@ impl Cache {
     }
 
     fn lookup(&self, command: &CommandDesc) -> Result<Option<Invocation>> {
-        let file = File::open(self.dir.join(command.cache_key()));
+        let file = File::open(self.key_dir.join(command.cache_key()));
         if let Err(ref e) = file {
             if e.kind() == ErrorKind::NotFound {
                 return Ok(None);
@@ -117,32 +131,49 @@ impl Cache {
         Ok(found)
     }
 
-    fn store(&self, invocation: &Invocation) -> Result<()> {
-        std::fs::create_dir_all(&self.dir)?;
-        let file = tempfile::NamedTempFile::new()?;
+    fn store(&self, invocation: &Invocation, ttl: &Duration) -> Result<()> {
+        let ttl_dir = self.data_dir.join(ttl.as_secs().to_string());
+        std::fs::create_dir_all(&ttl_dir)?;
+        std::fs::create_dir_all(&self.key_dir)?;
+        let file = tempfile::NamedTempFile::new_in(ttl_dir)?;
         Cache::serialize(BufWriter::new(&file), invocation)?;
-        file.persist(self.dir.join(invocation.command.cache_key()))?;
+        let (_, path) = file.keep()?;
+        // Roundabout approach to an atomic symlink replacement
+        // https://github.com/dimo414/bash-cache/blob/59bcad6/bash-cache.sh#
+        // Create and immediately destroy a file, to capture a (hopefully still) unique path name
+        // TODO eliminate this wasteful I/O
+        let tmp_symlink = tempfile::NamedTempFile::new()?.path().to_path_buf();
+        symlink(&path, &tmp_symlink)?;
+        std::fs::rename(&tmp_symlink, self.key_dir.join(invocation.command.cache_key()))?;
         Ok(())
     }
 }
 
 fn execute_subprocess(command: CommandDesc) -> Result<Invocation> {
+    let mut cmd = command.build_command();
+    let start = Instant::now();
     // TODO write to stdout/stderr while running, rather than after the process completes
     // See https://stackoverflow.com/q/66060139
-    let result = command.build_command().output()
+    let result = cmd.output()
         .with_context(|| format!("Failed to run command {}", command))?;
+    let runtime = start.elapsed();
+    let completed = SystemTime::now();
     Ok(Invocation {
         command,
         stdout: result.stdout,
         stderr: result.stderr,
         // TODO handle signals, see https://stackoverflow.com/q/66272686
         status: result.status.code().unwrap_or(126),
+        runtime,
+        completed,
     })
 }
 
 struct AppState {
     cache: Cache,
     command: CommandDesc,
+    ttl: Duration,
+    stale: Option<Duration>,
 }
 
 // Looks up a command in the cache, outputting its stdout/stderr if found. Otherwise executes
@@ -150,20 +181,28 @@ struct AppState {
 // exit status is returned, or an error message if either the cache could not be accessed or the
 // subprocess could not be run.
 fn run(state: AppState) -> Result<i32> {
-    let invocation = match state.cache.lookup(&state.command)? {
-        Some(cached) => {
-            // TODO TTL
-            // TODO warm cache
-            cached
-        },
+    let cached = state.cache.lookup(&state.command)?;
+    // Although atypical, it's possible for a caller to specify a different TTL than the command
+    // was cached with, so we exclude such results here instead of within the Cache.
+    let cached = cached.filter(|i| i.completed + state.ttl > SystemTime::now());
+    let invocation = match cached {
+        Some(cached) => cached,
         None => {
             let result = execute_subprocess(state.command)?;
-            state.cache.store(&result)?;
+            state.cache.store(&result, &state.ttl)?;
             result
-        },
+        }
     };
+
+    if let Some(stale) = state.stale {
+        if invocation.completed.elapsed().expect("BUG") > stale {
+            // TODO warm cache
+        }
+    }
+
     io::stdout().write_all(&invocation.stdout).unwrap();
     io::stderr().write_all(&invocation.stderr).unwrap();
+    // TODO delete expired data, see https://crates.io/crates/walkdir
     Ok(invocation.status)
 }
 
@@ -176,6 +215,14 @@ fn main() {
             .multiple(true)
             .last(true)
             .help("The command to run"))
+        .arg(Arg::with_name("ttl")
+            .long("time_to_live")
+            .alias("ttl")
+            .default_value("60s")
+            .help("Duration the cached result will be valid"))
+        .arg(Arg::with_name("stale")
+            .long("stale")
+            .help("Duration after which the cached result will be asynchronously refreshed"))
         .arg(Arg::with_name("cache_dir")
             .long("cache_dir")
             .help("The directory under which to persist cached invocations; defaults to the system's temp directory"))
@@ -184,9 +231,19 @@ fn main() {
             .help("If set all cached data will be scoped to this value, preventing collisions with commands cached with different scopes"))
         .get_matches();
 
+    // https://github.com/clap-rs/clap/discussions/2453
+    let stale = matches.value_of("stale")
+        .map(|v|
+            v.parse::<humantime::Duration>()
+                .map_err(|v| ::clap::Error::value_validation_auto(format!("The argument '{}' isn't a valid value", v)))
+                .unwrap_or_else(|e| e.exit())
+                .into());
+
     let state = AppState {
         cache: Cache::new(matches.value_of("cache_dir"), matches.value_of("cache_scope")),
         command: CommandDesc::new(matches.values_of("command").expect("Required").collect::<Vec<_>>()),
+        ttl: value_t_or_exit!(matches.value_of("ttl"), humantime::Duration).into(),
+        stale,
     };
 
     match run(state) {
