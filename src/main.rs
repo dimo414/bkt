d98960b200a25dff975f766633ea215ae109650e
Marcin Puc <marcin.e.puc@gmail.com>
2021-10-09 11:05:06 +0200

    Rewrite arg parsing in clap-derive

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::collections::{BTreeMap, BTreeSet};
-use std::ffi::OsStr;
+use std::ffi::OsString;
 use std::io::{self, Write};
 use std::path::PathBuf;
 use std::process::{Command, exit, Stdio};
 use std::time::{Duration};
 
 use anyhow::{Context, Result};
-use clap::{crate_description, crate_name, crate_version, value_t_or_exit, Arg, App};
+use clap::{AppSettings, Parser};
 
 use bkt::{CommandDesc, Bkt};
 
@@ -29,41 +29,46 @@ fn force_update_async() -> Result<()> {
 }
 
 // Runs bkt after main() handles flag parsing
-#[allow(clippy::too_many_arguments)]
-fn run(root_dir: Option<PathBuf>, discard_failures: bool, scope: Option<&str>,
-       mut command: CommandDesc, use_cwd: bool, env_keys: BTreeSet<&OsStr>, ttl: Duration,
-       stale: Option<Duration>, warm: bool, force: bool) -> Result<i32> {
+fn run(cli: Cli) -> Result<i32> {
+    let ttl: Duration = cli.ttl.into();
+    let stale: Option<Duration> = cli.stale.map(Into::into);
+
     assert!(!ttl.as_secs() > 0 || ttl.subsec_nanos() > 0, "--ttl cannot be zero"); // TODO use is_zero once stable
     if let Some(stale) = stale {
         assert!(!stale.as_secs() > 0 || stale.subsec_nanos() > 0, "--stale cannot be zero"); // TODO use is_zero once stable
         assert!(stale < ttl, "--stale must be less than --ttl");
     }
 
-    let mut bkt = match root_dir {
+    let mut bkt = match cli.cache_dir {
         Some(cache_dir) => Bkt::create(cache_dir)?,
         None => Bkt::in_tmp()?,
     };
-    bkt = bkt.discard_failures(discard_failures);
-    if let Some(scope) = scope {
-        bkt = bkt.scoped(scope.into());
+    bkt = bkt.discard_failures(cli.discard_failures);
+    if let Some(scope) = cli.scope {
+        bkt = bkt.scoped(scope);
     }
 
-    if use_cwd {
+    let mut command = CommandDesc::new(cli.command);
+
+    if cli.cwd {
         command = command.with_cwd()?;
     }
+
+    let env_keys: BTreeSet<_> = cli.env.into_iter().flatten().collect();
+
     if !env_keys.is_empty() {
-        let envs: BTreeMap<_,_> = std::env::vars_os()
-            // `as &OsStr` required per https://stackoverflow.com/q/65549983/113632
-            .filter(|(k,_)| env_keys.contains(k as &OsStr)).collect();
+        let envs: BTreeMap<_, _> = std::env::vars_os()
+            .filter(|(k, _)| env_keys.contains(k))
+            .collect();
         command = command.with_envs(&envs);
     }
 
-    if warm && !force {
+    if cli.warm && !cli.force {
         force_update_async()?;
         return Ok(0);
     }
 
-    let (invocation, age) = if force {
+    let (invocation, age) = if cli.force {
         (bkt.refresh(&command, ttl)?, Duration::from_secs(0))
     } else {
         bkt.retrieve(&command, ttl)?
@@ -80,89 +85,67 @@ fn run(root_dir: Option<PathBuf>, discard_failures: bool, scope: Option<&str>,
     Ok(invocation.exit_code())
 }
 
+#[derive(Parser)]
+#[clap(setting = AppSettings::DeriveDisplayOrder)]
+#[clap(about, version)]
+struct Cli {
+    /// The command to run
+    #[clap(required = true, last = true)]
+    command: Vec<OsString>,
+
+    /// Duration the cached result will be valid for
+    #[clap(long, default_value = "60s", visible_alias = "time-to-live")]
+    ttl: humantime::Duration,
+
+    /// Duration after which the result will be asynchronously refreshed
+    #[clap(long, conflicts_with = "warm")]
+    stale: Option<humantime::Duration>,
+
+    /// Asynchronously execute and cache the given command, even if it's already cached
+    #[clap(long)]
+    warm: bool,
+
+    /// Execute and cache the given command, even if it's already cached
+    #[clap(long, conflicts_with = "warm")]
+    force: bool,
+
+    /// Includes the current working directory in the cache key,
+    /// so that the same command run in different directories caches separately
+    #[clap(long, visible_alias = "use-working-dir")]
+    cwd: bool,
+
+    /// Includes the given environment variable in the cache key,
+    /// so that the same command run with different values for the given variables caches separately
+    #[clap(long, visible_alias = "use-environment")]
+    env: Option<Vec<OsString>>,
+
+    /// Don't cache invocations that fail (non-zero exit code).
+    /// USE CAUTION when passing this flag, as unexpected failures can lead to a spike in invocations
+    /// which can exacerbate ongoing issues, effectively a DDoS.
+    #[clap(long)]
+    discard_failures: bool,
+
+    /// If set, all cached data will be scoped to this value,
+    /// preventing collisions with commands cached with different scopes
+    #[clap(long)]
+    scope: Option<String>,
+
+    /// The directory under which to persist cached invocations;
+    /// defaults to the system's temp directory.
+    /// Setting this to a directory backed by RAM or an SSD, such as a tmpfs partition,
+    /// will significantly reduce caching overhead.
+    #[clap(long)]
+    cache_dir: Option<PathBuf>,
+}
+
 fn main() {
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .arg(Arg::with_name("command")
-            .required(true)
-            .multiple(true)
-            .last(true)
-            .help("The command to run"))
-        .arg(Arg::with_name("ttl")
-            .long("time-to-live")
-            .visible_alias("ttl")
-            .default_value("60s")
-            .help("Duration the cached result will be valid"))
-        .arg(Arg::with_name("stale")
-            .long("stale")
-            .takes_value(true)
-            .conflicts_with("warm")
-            .help("Duration after which the cached result will be asynchronously refreshed"))
-        .arg(Arg::with_name("warm")
-            .long("warm")
-            .takes_value(false)
-            .help("Asynchronously execute and cache the given command, even if it's already cached"))
-        .arg(Arg::with_name("force")
-            .long("force")
-            .takes_value(false)
-            .conflicts_with("warm")
-            .help("Execute and cache the given command, even if it's already cached"))
-        .arg(Arg::with_name("cwd")
-            .long("use-working-dir")
-            .visible_alias("cwd")
-            .takes_value(false)
-            .help("Includes the current working directory in the cache key, so that the same \
-                   command run in different directories caches separately"))
-        .arg(Arg::with_name("env")
-            .long("use-environment")
-            .visible_alias("env")
-            .takes_value(true)
-            .multiple(true)
-            .help("Includes the given environment variable in the cache key, so that the same \
-                   command run with different values for the given variables caches separately"))
-        .arg(Arg::with_name("discard-failures")
-            .long("discard-failures")
-            .help("Don't cache invocations that fail (non-zero exit code). USE CAUTION when \
-                      passing this flag, as unexpected failures can lead to a spike in invocations \
-                      which can exacerbate ongoing issues, effectively a DDoS."))
-        .arg(Arg::with_name("scope")
-            .long("scope")
-            .takes_value(true)
-            .help("If set, all cached data will be scoped to this value, preventing collisions \
-                   with commands cached with different scopes"))
-        .arg(Arg::with_name("cache_dir")
-            .long("cache-dir")
-            .takes_value(true)
-            .help("The directory under which to persist cached invocations; defaults to the \
-                   system's temp directory. Setting this to a directory backed by RAM or an SSD, \
-                   such as a tmpfs partition, will significantly reduce caching overhead."))
-        .get_matches();
-    let root_dir = matches.value_of("cache_dir").map(PathBuf::from);
-    let discard_failures = matches.is_present("discard-failures");
-    let scope = matches.value_of("scope");
-    let command = CommandDesc::new(matches.values_of_os("command").expect("Required").collect::<Vec<_>>());
-    let use_cwd = matches.is_present("cwd");
-    let env = matches.values_of_os("env").map(|e| e.collect()).unwrap_or_else(BTreeSet::new);
-    let ttl = value_t_or_exit!(matches.value_of("ttl"), humantime::Duration).into();
+    let cli = Cli::parse();
 
-    // https://github.com/clap-rs/clap/discussions/2453
-    let stale = matches.value_of("stale")
-        .map(|v|
-            v.parse::<humantime::Duration>()
-                .map_err(|v| ::clap::Error::value_validation_auto(
-                    format!("The argument '{}' isn't a valid value", v)))
-                .unwrap_or_else(|e| e.exit())
-                .into());
-    let warm = matches.is_present("warm");
-
-    let force = matches.is_present("force");
-
-    match run(root_dir, discard_failures, scope, command, use_cwd, env, ttl, stale, warm, force) {
+    match run(cli) {
         Ok(code) => exit(code),
         Err(msg) => {
             eprintln!("bkt: {:#}", msg);
             exit(127);
-        },
+        }
     }
 }
